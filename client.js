6698843e67e7d3500116459783cb9ba6f1787af2
lgaeun <gaeungaeun4837@gmail.com>
2021-07-03 06:53:47 +0900

    ✨query 최적화 - server.js를 movies 폴더로 분리

---
+++ client.js
@@ -0,0 +1,5 @@
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient(); //ready to use prisma client
+
+export default client;
---
+++ schema.js
@@ -0,0 +1,21 @@
+//bring all queries, typeDefs, mutations alltogether
+import {
+  loadFilesSync,
+  makeExecutableSchema,
+  mergeResolvers,
+  mergeTypeDefs,
+} from "graphql-tools";
+
+//loadFileSync: default export를 가져온다. export를 하지 않으면 error 발생.
+//anyfolder/anyfilename.typeDefs.js
+const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
+const loadedResolvers = loadFilesSync(
+  `${__dirname}/**/*.{queries, mutations}.js` //this is called glob! (=pattern language)
+);
+
+const typeDefs = mergeTypeDefs(loadedTypes);
+const resolvers = mergeResolvers(loadedResolvers);
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+export default schema;
