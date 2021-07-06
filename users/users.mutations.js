98dcfe12f336866a460d60559d804230c4689ea4
lgaeun <gaeungaeun4837@gmail.com>
2021-07-06 11:28:44 +0900

    feat: 유저 계정 db에 생성코드 추가

---
+++ users/users.mutations.js
@@ -0,0 +1,29 @@
+import { async } from "regenerator-runtime";
+import client from "../client";
+
+export default {
+  Mutation: {
+    createAccount: async (
+      _,
+      { firstName, lastName, username, email, password }
+    ) => {
+      //check if username or email are already on DB.
+      const existingUser = await client.user.findFirst({
+        //send filters where having same username or email
+        where: {
+          OR: [
+            {
+              username,
+            },
+            {
+              email,
+            },
+          ],
+        },
+      });
+      console.log(existingUser);
+      //hash password
+      //save and return the user
+    },
+  },
+};
