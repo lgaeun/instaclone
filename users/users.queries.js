b589248eeca694b844ad28664d4560b345c55989
lgaeun <gaeungaeun4837@gmail.com>
2021-08-24 16:03:18 +0900

    #4.3 see Profile

--- users/users.queries.js
+++ users/users.queries.js
@@ -1,3 +1,11 @@
+import client from "../client";
+
 export default {
-  Query: {},
+  Query: {
+    seeProfile: (_, {username}) => client.user.findUnique({ //@unique인 값으로만 find
+      where:{
+        username,
+      }
+    })
+  },
 };
