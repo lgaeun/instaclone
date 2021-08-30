d2b447c3872b6402bfd2d176de98b0f1e671c154
lgaeun <gaeungaeun4837@gmail.com>
2021-08-31 08:18:53 +0900

    4.7 architecture 정리

--- schema.js
+++ schema.js
@@ -9,9 +9,7 @@ import {
 //loadFileSync: default export를 가져온다. export를 하지 않으면 error 발생.
 //anyfolder/anyfilename.typeDefs.js
 const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
-const loadedResolvers = loadFilesSync(
-  `${__dirname}/**/*.{queries,mutations}.js` //this is called glob! (=pattern language)
-);
+const loadedResolvers = loadFilesSync(`${__dirname}/**/*.resolvers.js`);
 
 const typeDefs = mergeTypeDefs(loadedTypes);
 const resolvers = mergeResolvers(loadedResolvers);
---
+++ users/seeProfile/seeProfile.resolvers.js
@@ -0,0 +1,13 @@
+import client from "../../client";
+
+export default {
+  Query: {
+    seeProfile: (_, { username }) =>
+      client.user.findUnique({
+        //@unique인 값으로만 find
+        where: {
+          username,
+        },
+      }),
+  },
+};
