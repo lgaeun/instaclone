b2e992221665cc77e47a24fd53dd5ed5789257b6
lgaeun <gaeungaeun4837@gmail.com>
2021-10-27 07:51:14 +0900

    apollo server가 schema 생성하도록 변경

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require("dotenv").config(); //최상단에 가상환경 load
 import { ApolloServer, gql } from "apollo-server";
-import schema from "./schema";
+import schema, { typeDefs, resolvers } from "./schema";
 import { getUser, protectedResolver } from "./users/users.utils";
 
 const server = new ApolloServer({
+  typeDefs,
+  resolvers,
   schema,
   context: async ({ req }) => {
     return {
