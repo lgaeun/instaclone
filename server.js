c0c2be5d2e5c1bd5a232eed13a98b8ddf0b30063
lgaeun <gaeungaeun4837@gmail.com>
2021-10-28 07:30:58 +0900

    change apollo server -> apollo express serever

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 require("dotenv").config(); //최상단에 가상환경 load
-import { ApolloServer, gql } from "apollo-server";
+import express from "express";
+import logger from "morgan";
+import { ApolloServer } from "apollo-server-express";
 import schema, { typeDefs, resolvers } from "./schema";
 import { getUser, protectedResolver } from "./users/users.utils";
+import { graphql } from "graphql";
 
+const PORT = process.env.PORT;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  schema,
+  // schema,
   context: async ({ req }) => {
     return {
       loggedInUser: await getUser(req.headers.token),
-      protectedResolver,
+      // protectedResolver,
     };
   },
 });
 
-const PORT = process.env.PORT;
-
-server
-  .listen(PORT)
-  .then(() => console.log(`Server is running on http://localhost:${PORT}/ ✅`));
+const app = express();
+app.use(logger("tiny"));
+server.applyMiddleware({ app });
+app.listen({ port: PORT }, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}/graphql ✅`);
+});
