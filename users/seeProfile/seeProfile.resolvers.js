77b933d1f5ea72436226f2a2d7635ff0cd628670
lgaeun <gaeungaeun4837@gmail.com>
2021-11-01 23:13:16 +0900

    주석제거

--- users/seeProfile/seeProfile.resolvers.js
+++ users/seeProfile/seeProfile.resolvers.js
@@ -4,15 +4,14 @@ export default {
   Query: {
     seeProfile: (_, { username }) =>
       client.user.findUnique({
-        //@unique인 값으로만 find
         where: {
           username,
         },
         //relation이 있는 경우 include를 통해서 전체 보여줄 수 있음
-        // include: {
-        //   followers: true,
-        //   following: true,
-        // },
+        include: {
+          following: true,
+          followers: true,
+        },
       }),
   },
 };
