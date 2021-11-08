901e52e1f9290b43a35f2abc90a01bd3632e9f20
lgaeun <gaeungaeun4837@gmail.com>
2021-11-09 07:54:48 +0900

    create hashtag

--- photos/upload/uploadPhoto.resolvers.js
+++ photos/upload/uploadPhoto.resolvers.js
@@ -1,10 +1,35 @@
+import client from "../../client";
 import { protectedResovler } from "../../users/users.utils";
 
 export default {
   Mutation: {
     uploadPhoto: protectedResovler(
       async (_, { file, caption }, { loggedInUser }) => {
-        const hashtag = caption.match(/#[\w]+/g);
+        let hashtagObj = [];
+        if (caption) {
+          const hashtags = caption.match(/#[\w]+/g);
+          hashtagObj = hashtags.map((hashtag) => ({
+            where: { hashtag },
+            create: { hashtag },
+          }));
+          console.log(hashtagObj);
+        }
+        return client.photo.create({
+          data: {
+            file,
+            caption,
+            user: {
+              connect: {
+                id: loggedInUser.id,
+              },
+            },
+            ...(hashtagObj.length > 0 && {
+              hashtags: {
+                connectOrCreate: hashtagObj,
+              },
+            }),
+          },
+        });
       }
     ),
   },
